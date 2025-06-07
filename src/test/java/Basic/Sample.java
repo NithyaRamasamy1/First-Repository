2f1f6629ad621426f627f89171dc48499f6cea4a
welcome <welcome@DESKTOP-QHRBAV4>
2025-06-08 05:05:22 +0530

    hello

--- src/test/java/Basic/Sample.java
+++ src/test/java/Basic/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Well done Nithu");
+		System.out.println("New password updated");
 	}
 
 }
